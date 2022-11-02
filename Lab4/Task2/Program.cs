1e83408d0d3fc47acd7e9656a77c389d6af4ef63
Denis <deniskurkavol@gamil.com>
2022-11-02 23:41:56 +0200

    lab4
    lab4

---
+++ Lab4/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Heir1 heir1 = new Heir1();
+            Heir2 heir2 = new Heir2();
+            Heir3 heir3 = new Heir3();
+            A a = new A(heir1, heir2, heir3);
+            double y = heir1.Math(1, 3);
+            Console.WriteLine(y);
+        }
+    }
+}
